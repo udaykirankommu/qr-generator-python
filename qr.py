798f9eea19bacf1e971cf1592bed6d727f45cde7
udaykirankommu <udaykirankommu24@gmail.com>
2026-07-14 13:04:43 +0530

    qr-succeessfully

--- qr.py
+++ qr.py
@@ -1,2 +1,11 @@
 import qrcode
-print
+print("******* QR Code Generated *******")
+d = input("Enter text or URL: ")
+img=qrcode.make(d)
+
+file=input("Enter file name(without .png): ")
+
+img.save(file + ".png")
+
+print(f"\n QR Code generated successfully!")
+print(f"saved as: {file}.png")
